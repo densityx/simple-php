75c0a083cd66713e7fdb845f8c2aff5f53015b06
Ahmad Aziz <ahmadaziz0441@gmail.com>
2021-10-10 07:37:41 +0800

    add PHP operators

---
+++ operators.php
@@ -0,0 +1,15 @@
+<?php
+
+// php opertators
+
+$a = 1;
+$b = 2;
+
+$c = +$a; // identity
+$c = -$a; // negation
+$c = $a + $b; // addition
+$c = $a - $b; // subtraction
+$c = $a * $b; // multiplication
+$c = $a / $b; // division
+$c = $a % $b; // modulo
+$c = $a ** $b; // exponentiation
